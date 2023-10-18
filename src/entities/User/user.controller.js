352e58dc8e4029aa95321d052ea5d4f18b9641ec
Julio Fiamoncini <juliogustavofi@gmail.com>
2023-10-18 16:56:02 -0300

    feat: update user info

--- src/entities/User/user.controller.js
+++ src/entities/User/user.controller.js
@@ -1,4 +1,4 @@
-import { createUser, findAllUsers, verifyUser, findById } from './user.service.js'
+import { createUser, findAllUsers, verifyUser, findById, updateUser } from './user.service.js'
 
 export async function createUserController (req, res) {
   try {
@@ -48,4 +48,26 @@ export async function findByIdController (req, res) {
   } catch (error) {
     res.status(500).send({ message: error.message })
   }
+}
+
+export async function editUser (req, res) {
+  const { username, email, password, fullName, avatar, contact } = req.body
+  const { id } = req.params
+
+  if (!username && !email && !password && !fullName && !avatar && !contact) {
+    return res.status(400).send({ message: 'Please fill in at least 1 fild to change some info' })
+  }
+
+  const updatedUser = await updateUser(id, {
+    username,
+    email,
+    password,
+    fullName,
+    avatar,
+    contact
+  })
+
+  if (!updatedUser) return res.status(404).send({ message: 'An error occurred when trying to change the information. Try again later' })
+
+  res.status(200).send({ message: 'User successfully updated' })
 }
--- src/entities/User/user.routes.js
+++ src/entities/User/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { createUserController, findAllUsersController, findByIdController } from './user.controller.js'
+import { createUserController, findAllUsersController, findByIdController, editUser } from './user.controller.js'
 import { validateId } from './user.middleware.js'
 
 const router = Router()
@@ -8,5 +8,6 @@ const router = Router()
 router.post('/', createUserController)
 router.get('/', findAllUsersController)
 router.get('/:id', validateId, findByIdController)
+router.patch('/:id', validateId, editUser)
 
 export default router
