0fb6472983b2e1b3113a67ddd8311cc734cb2548
Julio Fiamoncini <juliogustavofi@gmail.com>
2023-10-22 19:15:09 -0300

    doc: add swagger docs

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import 'dotenv/config'
 import express from 'express'
 
 import { connectToMongo } from './database/db.config.js'
+
 import userRoutes from './routes/user.routes.js'
 import eventRoutes from './routes/event.routes.js'
 import loginRoutes from './routes/login.routes.js'
+import swaggerRoutes from './routes/swagger.routes.js'
 
 const app = express()
 
@@ -12,7 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/event', eventRoutes)
-app.use('/auth', loginRoutes)
+app.use('/login', loginRoutes)
+app.use('/doc', swaggerRoutes)
 app.use('/', userRoutes)
 
 app.listen(process.env.PORT || 8080, () => {
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -23,11 +23,11 @@ router.get('/', findAllEventsController)
 router.get('/latest', lastestEventController)
 router.get('/search', searchByTitleController)
 router.get('/ByUser', authMiddleware, byUserController)
-router.get('/:id', authMiddleware, validateId, findByIdController)
 router.patch('/join/:id', authMiddleware, validateId, joinEventController)
-router.patch('/:id', authMiddleware, validateId, updateController)
 router.patch('/comment/:id', authMiddleware, validateId, addCommentController)
 router.patch('/comment/:eventId/:commentId', authMiddleware, removeCommentController)
+router.get('/:id', authMiddleware, validateId, findByIdController)
+router.patch('/:id', authMiddleware, validateId, updateController)
 router.delete('/:id', authMiddleware, validateId, deleteController)
 
 export default router
