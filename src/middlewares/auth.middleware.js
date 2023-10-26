37e3055546b60d6bb57582583b4dc804c95cfbb7
Julio Fiamoncini <juliogustavofi@gmail.com>
2023-10-26 16:04:52 -0300

    revert(ec39ce8): the code look weird

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,13 +1,30 @@
-import authService from '../services/auth.service.js'
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+
+import authRepository from '../repositories/auth.repository.js'
 
 export async function authMiddleware (req, res, next) {
-  const { authorization } = req.headers
-
   try {
-    const userId = await authService.auth(authorization)
-    req.userId = userId
-    return next()
+    const { authorization } = req.headers
+    if (!authorization) return res.status(401).send({ message: 'The token was not informed!' })
+
+    const parts = authorization.split(' ')
+    if (parts.length !== 2) return res.status(401).send({ message: 'Access denied' })
+
+    const [bearer, token] = parts
+    if (bearer !== 'Bearer') return res.status(401).send({ message: 'Access denied' })
+
+    jwt.verify(token, process.env.JWT_SECRET, async (error, decode) => {
+      if (error) return res.status(400).send({ message: 'Invalid Token!' })
+
+      const user = await authRepository.findById(decode._id)
+      if (!user || !user._id) return res.status(400).send({ message: 'Invalid Token!' })
+
+      req.userId = decode._id
+
+      return next()
+    })
   } catch (error) {
-    return res.status(500).send({ message: error.message })
+    res.status(500).send({ message: error.message })
   }
 }
